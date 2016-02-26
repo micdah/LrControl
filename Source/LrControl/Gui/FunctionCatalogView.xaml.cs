153e476a98e8294bfd0c50406b823e52b31180ba
Michael Dahl <micdah@gmail.com>
2016-02-26 16:04:03 +0100

    Moved parts of the mainwindow into appropriate user controls, mainly just to reduce ui file sizes

---
+++ Source/LrControl/Gui/FunctionCatalogView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using micdah.LrControl.Mapping.Catalog;
+
+namespace micdah.LrControl.Gui
+{
+    /// <summary>
+    ///     Interaction logic for FunctionCatalogView.xaml
+    /// </summary>
+    public partial class FunctionCatalogView
+    {
+        public FunctionCatalogView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty FunctionCatalogProperty = DependencyProperty.Register(
+            "FunctionCatalog", typeof (FunctionCatalog), typeof (FunctionCatalogView), new PropertyMetadata(default(FunctionCatalog)));
+
+        public FunctionCatalog FunctionCatalog
+        {
+            get { return (FunctionCatalog) GetValue(FunctionCatalogProperty); }
+            set { SetValue(FunctionCatalogProperty, value); }
+        }
+    }
+}
---
+++ Source/LrControl/Gui/ModuleGroupView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using micdah.LrControl.Mapping;
+
+namespace micdah.LrControl.Gui
+{
+    /// <summary>
+    ///     Interaction logic for ModuleGroupView.xaml
+    /// </summary>
+    public partial class ModuleGroupView
+    {
+        public ModuleGroupView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ModuleGroupProperty = DependencyProperty.Register(
+            "ModuleGroup", typeof (ModuleGroup), typeof (ModuleGroupView), new PropertyMetadata(default(ModuleGroup)));
+
+        public ModuleGroup ModuleGroup
+        {
+            get { return (ModuleGroup) GetValue(ModuleGroupProperty); }
+            set { SetValue(ModuleGroupProperty, value); }
+        }
+
+    }
+}
