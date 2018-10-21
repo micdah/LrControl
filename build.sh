2ecb7bdbf7e749850cb64d6091792f928491e385
Michael Dahl <micdah@gmail.com>
2018-10-21 13:28:58 +0200

    Updated .sln file with new placement

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 Configuration=$1
-Output="../../Build/${Configuration}/LrControl.lrplugin/"
+Output="../Build/${Configuration}/LrControl.lrplugin/"
 Project=""
 
-dotnet build LrControl.Plugin/LrControl.Plugin.csproj --configuration ${Configuration}
-dotnet publish LrControl.Console/LrControl.Console.csproj --configuration ${Configuration} --runtime osx-x64 --self-contained --output ${Output}/osx/
-dotnet publish LrControl.Console/LrControl.Console.csproj --configuration ${Configuration} --runtime win-x64 --self-contained --output ${Output}/win/
+dotnet build src/LrControl.Plugin/LrControl.Plugin.csproj --configuration ${Configuration}
+dotnet publish src/LrControl.Console/LrControl.Console.csproj --configuration ${Configuration} --runtime osx-x64 --self-contained --output ${Output}/osx/
+dotnet publish src/LrControl.Console/LrControl.Console.csproj --configuration ${Configuration} --runtime win-x64 --self-contained --output ${Output}/win/
