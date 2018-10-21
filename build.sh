045c38cf084eaa6c1e473a29ab82a2dfe9ab2def
Michael Dahl <micdah@gmail.com>
2018-10-21 13:32:54 +0200

    Updated build.sh with new paths

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 Configuration=$1
 Output="../Build/${Configuration}/LrControl.lrplugin/"
-Project=""
 
-dotnet build src/LrControl.Plugin/LrControl.Plugin.csproj --configuration ${Configuration}
+dotnet build src/LrControl.LrPlugin/LrControl.LrPlugin.csproj --configuration ${Configuration}
 dotnet publish src/LrControl.Console/LrControl.Console.csproj --configuration ${Configuration} --runtime osx-x64 --self-contained --output ${Output}/osx/
 dotnet publish src/LrControl.Console/LrControl.Console.csproj --configuration ${Configuration} --runtime win-x64 --self-contained --output ${Output}/win/
